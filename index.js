8736d082c082e986a6c046d8bde6b88fd7984704
ubiratanfilho <ubfilho16@gmail.com>
2023-02-25 09:58:47 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/// Parte 1 - Declaração de variáveis
+
+// // declarando constantes
+// const a = 10
+// const b = 20
+
+// // declarando variáveis locais
+// let nome = 'João'
+// let idade = 20
+
+// // declarando variáveis globais
+// var c = 10
+// var d = 20
+
+// // exibindo valores
+// console.log(a)
+
+// // condição
+// if (a > b) {
+//     console.log('a é maior que b')
+//     }
+// else {
+//     console.log('b é maior que a')
+//     }
+
+/// Parte 2
+
+let linguagem = 'JavaScript'
+console.log('Aprendendo ' + linguagem)
+
+console.log(`Aprendendo ${linguagem}`)
